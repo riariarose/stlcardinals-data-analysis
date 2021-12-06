ddc2144ddbb2f6763ca3f226cfb3887b4f6efe60
riariarose <maria68@live.missouristate.edu>
2021-12-05 20:03:16 -0600

    added more files

--- src/batting.py
+++ src/batting.py
@@ -3,13 +3,6 @@ CSC535-FA2021
 Data Mining
 Final Group Project
 
-Maria's deliverables: 
-The top 6 best performing players per position based on game statistics 
-Positions: Pitcher, Catcher, 1B, 2B, 3B, Short stop, Right fielder, Left fielder, Center fielder and Designated Hitter
-
-Column names in the "full_roster.csv" file
-Name,Age,Country,B,T,Ht,Wt,DoB,Yrs,G,GS,Batting,Defense,P,C,1B,2B,3B,SS,LF,CF,RF,OF,DH,PH,PR,WAR,Status
-
 Column names in the "team_batting.csv" file
 Rank,Position,Name,Age,Games,PA,AB,Runs,Hits,2B,3B,HR,RBI,SB,CS,BB,SO,BA,OBP,SLG,OPS,OPS+,TB,GDP,HBP,SH,SF,IBB
 ------------------------------------------------'''
@@ -21,14 +14,7 @@ def get_data(fileName):
     df = pd.read_csv(fileName)
     return df
 
-#preprocessing the full_roster.csv file
-def preprocess_roster(df):
-    roster = df.drop(['Country', 'Ht', 'Wt', 'DoB', 'Yrs', 'WAR', 'Status'], axis=1)
-    print(roster)
-    return roster
-
-
-#preprocessing the team_batting.csv file
+# preprocessing the team_batting.csv file
 def preprocess_batting(df):
     # batting = df.drop(['Country', 'Ht', 'Wt', 'DoB', 'Yrs', 'WAR', 'Status'], axis=1)
     ranks = pd.DataFrame(df.iloc[:, 0:4])
@@ -39,12 +25,7 @@ def preprocess_batting(df):
 def main():
 
     batting_file = "team_batting.csv"
-    roster_file = "full_roster.csv"
-
     batting_df = get_data(batting_file)
-    roster_df = get_data(roster_file)
-    
-    preprocess_roster(roster_df)
     preprocess_batting(batting_df)
 
 main()
