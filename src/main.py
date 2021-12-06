ca81a8e7062121d6f1ba73da5174e77d6dc31bdf
riariarose <maria68@live.missouristate.edu>
2021-12-05 18:56:35 -0600

    updated file directory

--- src/main.py
+++ src/main.py
@@ -1,2 +1,22 @@
+'''---------------------------------------------
+Maria Harrison
+CSC535-Data Mining
+Dr. Saquer
+Final Group Project
+
+Maria's deliverables:
+------------------------------------------------'''
+
 import pandas as pd
 import numpy as np
+
+def get_data(fileName):
+    df = pd.read_csv(fileName)
+    return df
+
+def main():
+    batting_file = "team_batting.csv"
+    roster_file = "full_roster.csv"
+
+    batting_data = get_data(batting_file)
+    roster_data = get_data(roster_file)
